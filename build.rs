e3555fde878edfd935706e66f328fe1a5f52688e
Giang Minh <giangndm@ows.vn>
2023-06-19 08:52:10 +0700

    fixing build in arm

--- build.rs
+++ build.rs
@@ -112,24 +112,28 @@ fn build() {
     }
 
     if host == target {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         if !std::is_x86_feature_detected!("avx") {
             cmake.define("OPUS_X86_MAY_HAVE_AVX", "OFF")
                  .define("OPUS_X86_PRESUME_AVX", "OFF")
                  .define("AVX_SUPPORTED", "OFF");
         }
 
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         if !std::is_x86_feature_detected!("sse4.1") {
             cmake.define("OPUS_X86_MAY_HAVE_SSE4_1", "OFF")
                  .define("OPUS_X86_PRESUME_SSE4_1", "OFF")
                  .define("SSE4_1_SUPPORTED", "OFF");
         }
 
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         if !std::is_x86_feature_detected!("sse2") {
             cmake.define("OPUS_X86_MAY_HAVE_SSE2", "OFF")
                  .define("OPUS_X86_PRESUME_SSE2", "OFF")
                  .define("SSE2_SUPPORTED", "OFF");
         }
 
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         if !std::is_x86_feature_detected!("sse") {
             cmake.define("OPUS_X86_MAY_HAVE_SSE", "OFF")
                  .define("OPUS_X86_PRESUME_SSE", "OFF")
